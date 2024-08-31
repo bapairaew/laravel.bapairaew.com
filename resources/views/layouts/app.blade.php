36f1e98f8365f44b1cf6967c9ea08ded0d765439
dom <iam@bapairaew.com>
2024-08-31 16:41:05 +0700

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Dom.')</title>
     <meta name="description" content="@yield('description', 'Some random website.')">
+    <meta name="csrf_token" value="{{ csrf_token() }}" />
     @vite('resources/css/app.css')
     @vite('resources/views/components/navigations/root-navigation.css')
     @stack('styles')
