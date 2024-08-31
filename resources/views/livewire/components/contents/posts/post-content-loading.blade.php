daacd17198db0132b8f6ffed2f916e08f0acb739
dom <iam@bapairaew.com>
2024-08-31 23:07:51 +0700

    typo

--- resources/views/livewire/components/contents/posts/post-content-loading.blade.php
+++ resources/views/livewire/components/contents/posts/post-content-loading.blade.php
@@ -1,9 +1,9 @@
 <div class="prose lg:prose-md dark:prose-invert mx-auto">
     <h1 class="pt-8 m-0">
-        <<x-skeletons.text-skeleton class="w-10/12" />
+        <x-skeletons.text-skeleton class="w-10/12" />
     </h1>
     <span class="block pt-8 text-md">
-        <<x-skeletons.text-skeleton class="w-9/12" />
+        <x-skeletons.text-skeleton class="w-9/12" />
     </span>
     <div class="pt-4">
         <p>
